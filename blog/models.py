2a1f413814b6d7e64f360b5f5fceaebde28cccb4
hyhyhy38371 <git@github.com:hyhyhy38371>
2022-10-27 11:58:11 +0900

    블로그 목록에 a 태그 넣기

--- blog/models.py
+++ blog/models.py
@@ -7,5 +7,9 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def get_absolute_url(self):
+        #TODO: 향후에는 장고의 URL REVERSE 기능을 사용하세용!
+        return "/{self.pk}/"
+    
     def __str__(self):
         return f'[{self.pk}]{self.title}'
